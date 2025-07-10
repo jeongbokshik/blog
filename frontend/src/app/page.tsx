e2caa55cea5e309df4b9e498c1a7617d85e13585
roadpienode@roadpie.cafe24app.com:roadpienode_roadpie <roadpienode@cafe24.com>
2025-07-10 14:56:34 +0900

    test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
+          <li>안녕하세요</li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
